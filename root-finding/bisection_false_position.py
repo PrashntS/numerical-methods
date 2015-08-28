8b5f6c5827559a6aed854de8f816ed11816150f2
Prashant Sinha <prashant@ducic.ac.in>
2015-08-28 11:43:11 +0530

    Another eg.

--- root-finding/bisection_false_position.py
+++ root-finding/bisection_false_position.py
@@ -9,6 +9,12 @@ def f0(x):
 def f1(x):
     return (x**3 + (2 * x**2) - (3 * x) - 1)
 
+def fluid(x):
+    rho_f = 0.890
+    rho_o = 0.120
+    r     = 5
+    return (((rho_f / 3) * x**3) - (r * rho_f * x**2) + ((4 * r**3 * rho_o) / 3))
+
 class num_method(object):
     def __init__(self, f, r = None):
         self.f = f
@@ -20,6 +26,7 @@ class num_method(object):
     def choose_interval(self, left, right):
         left_multiple  = self.f(left[0])  * self.f(left[1])
         right_multiple = self.f(right[0]) * self.f(right[1])
+
         if left_multiple < 0:
             return left
 
@@ -27,10 +34,10 @@ class num_method(object):
             return right
 
         else:
-            raise Exception
+            return None
 
     def routine(self, interval):
-        self.temp_iter   += 1
+        self.temp_iter += 1
         approx_root  = self.root_approx(interval[0], interval[1])
 
         if self.round:
@@ -40,31 +47,38 @@ class num_method(object):
         right        = [approx_root, interval[1]]
         new_interval = self.choose_interval(left, right)
 
-        if self.temp_iter >= self.MAX_ITER:
+        if self.root == approx_root:
             iter_count = self.temp_iter
             self.temp_iter = 0
             return self.root, iter_count
-
+        elif self.temp_iter >= self.MAX_ITER:
+            iter_count = self.temp_iter
+            self.temp_iter = 0
+            return self.root, iter_count
+        elif new_interval is None:
+            iter_count = self.temp_iter
+            self.temp_iter = 0
+            return self.root, iter_count
         else:
             self.root = approx_root
             return self.routine(new_interval)
 
 class bisection(num_method):
     def root_approx(self, lower, upper):
-        return (lower + upper) / 2
+        root = (lower + upper) / 2
+        print ("Method: Bisection -> Count: {0}, Root: {1}".format(self.temp_iter, root))
+        return root
 
 class false_position(num_method):
     def __init__(self, f, r = None):
         self.pn = []
         self.er = []
-        self.count = 0
         super(false_position, self).__init__(f, r)
 
     def root_approx(self, a, b):
         root = (b - self.f(b) * ((b - a) / (self.f(b) - self.f(a))))
-        self.count += 1
         self.pn.append(root)
-        print ("Count: {0}, Root: {1} -> Error: {2}".format(self.count, root, self.error(self.count - 1)))
+        print ("Method: False Posn -> Count: {0}, Root: {1} -> Error: {2}".format(self.temp_iter, root, self.error(self.temp_iter - 1)))
         return root
 
     def error(self, n):
@@ -79,5 +93,17 @@ class false_position(num_method):
             return None
 
 if __name__ == "__main__":
-    obj2 = false_position(f1, 20)
-    print (obj2.routine([1, 2]))
+    # obj2 = false_position(f1, 12)
+    # obj2.routine([1, 2])
+
+    # print("LOL")
+
+    # obj1 = bisection(f1, 12)
+    # obj1.routine([1, 2])
+    #
+    ob = false_position(fluid, 12)
+
+
+    ob.routine([0, 3])
+
+    print(fluid(3))
