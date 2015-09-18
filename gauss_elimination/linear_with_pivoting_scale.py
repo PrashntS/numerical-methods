5141ac8bc8dfc954cfe59b50b7fb7f4e06d075f3
Prashant Sinha <prashant@ducic.ac.in>
2015-09-18 13:11:33 +0530

    Pivot Done

--- gauss_elimination/linear_with_pivoting_scale.py
+++ gauss_elimination/linear_with_pivoting_scale.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 A = [
-    [5,  7, 14, -8],
-    [2, -2, -1,  2],
-    [3,  1,  4, -1],
-    [1,  3,  2,  4]
+    [1,  1,  1,  1],
+    [1,  1,  2,  3],
+    [-1, 0,  2,  1],
+    [3,  2, -1,  0]
 ]
 
-B = [20, 1, 7, -4]
+B = [1, 2, 1, 1]
 
 
 class Matrix(object):
@@ -57,7 +57,7 @@ class Matrix(object):
         mat = self.AUG if target is "aug" else self.A
 
         #: Swap the rows
-        mat[_to] = [mat[_to][_] + constant * mat[_from][_] for _ in range(len(mat[_from]))]
+        mat[_to] = [self._mul(mat[_to][_], round(constant, 3) * mat[_from][_]) for _ in range(len(mat[_from]))]
 
         if push:
             if target is "aug":
@@ -80,24 +80,33 @@ class Matrix(object):
             We have the Row
             """
             row = self.AUG[offset][offset:]
-            print(self.AUG)
-            #print(row[0])
             for i in range(1, len(self.AUG) - offset):
                 const = -1 * self.AUG[offset + i][offset:][0] / row[0]
                 self.add_rows(offset, offset + i, const)
 
-            # const = -1 * self.AUG[offset + 2][offset:][0] / row[0]
-            # self.add_rows(offset, offset + 2, const)
-            # const = -1 * self.AUG[offset + 3][offset:][0] / row[0]
-            # self.add_rows(offset, offset + 3, const)
+            self.scaled_pivot()
 
-    def zero_transpose(self):
+    def scaled_pivot(self):
         """
         """
-        pass
+        for offset in range(len(self.AUG)):
+            column = [(_, self.AUG[_][offset:][0]) for _ in range(offset, len(self.AUG))]
+            col_lis = sorted(column, key = lambda x: abs(x[1] / self.scale_vector[offset]), reverse=True)
 
+            self.AUG = self.AUG[0:offset] + [self.AUG[_] for (_, __) in col_lis]
 
-a = Matrix(A, B)
+    def _mul(self, a, b):
+        res = a + b
 
+        if abs(res) < 0.0000000005:
+            return 0
+        else:
+            return round(res, 10)
+
+a = Matrix([
+    [0.7, 1725],
+    [0.4352, -5.433]
+    ], [1739, 3.271])
 a.upper_triangle()
 print(a.AUG)
+print(a.AUG[1][2] / a.AUG[1][1])
